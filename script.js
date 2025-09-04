d8133ffe949ccc8790935ea077bb51fca8fd807f
sidfakenews986 <bisonsicolo@hotmail.com>
2025-09-04 18:45:08 +0100

    feat: initial commit for script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleButton = document.getElementById('toggle-theme');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    toggleButton.innerText = document.body.classList.contains('dark-mode') ? 'Heller Modus Aktivieren': 'Dunkelmodus Aktivieren';
+});
