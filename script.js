d437ca81f0072dfd355843ce6d0be9769a963885
sidfakenews986 <bisonsicolo@hotmail.com>
2025-09-04 18:45:18 +0100

    Fix typo in the innerText of the toggle button for clarity.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const toggleButton = document.getElementById('toggle-theme');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-    toggleButton.innerText = document.body.classList.contains('dark-mode') ? 'Heller Modus Aktivieren': 'Dunkelmodus Aktivieren';
+    toggleButton.innerText = document.body.classList.contains('dark-mode') ? 'Heller Modus Aktivieren' : 'Dunkelmodus Aktivieren';
 });
